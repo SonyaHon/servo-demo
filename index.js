fcfe574ecee80f059fe3d9d93baada6e23a0d16d
sonyahon <volkov030@gmail.com>
2020-10-11 18:30:26 +0300

    asd

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const Gpio = require("pigpio").Gpio;
+const readline = require('readline');
 
 console.log("init servos")
 const headMotor = new Gpio(14, { mode: Gpio.OUTPUT });
 const bodyMotor = new Gpio(15, { mode: Gpio.OUTPUT });
 
 console.log("Just set servos to zero");
-setInterval(() => {
-  headMotor.servoWrite(2300);
-  bodyMotor.servoWrite(800);
-}, 1000);
+headMotor.servoWrite(2300);
+bodyMotor.servoWrite(800);
+
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const cmdmap = {
+    'mvhead': (value) => {
+        headMotor.servoWrite(Number(value));
+    },
+    'mvbody': (value) => {
+        bodyMotor.servoWrite(Number(value));
+    }
+}
+
+rl.on('line', (input) => {
+    const [cmd, val] = input.split(' ');
+    cmdmap[cmd](val);
+});
+
+setInterval(() => {}, 1000);
